c7d0717521818102e4b2c2a76adddc34eeb2e094
Radeonares32 <bugra.linux.js@gmail.com>
2023-02-15 15:27:30 +0300

    called callConstructor

--- kernel.cpp
+++ kernel.cpp
@@ -13,9 +13,9 @@ extern "C" constructor start_ctors;
 extern "C" constructor end_ctors;
 extern "C" void callConstructors()
 {
-    for (constructor *i = &start_ctors; i != end_ctors; i++)
+    for (constructor* i = &start_ctors; i != end_ctors; i++)
     {
-       
+       (*i)();
     }
 }
 
