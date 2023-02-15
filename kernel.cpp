8a39f42fd4c754bb3e9c6526252276b9b1c30099
Radeonares32 <bugra.linux.js@gmail.com>
2023-02-15 15:24:47 +0300

    kernel ctors memory address created

--- kernel.cpp
+++ kernel.cpp
@@ -1,10 +1,27 @@
-void printf(char* str) {
-    unsigned short* VideoMemory = (unsigned short*)0xb8000;
-    for(int i=0;str[i] != '\0'; ++i) {
+void printf(char *str)
+{
+    unsigned short *VideoMemory = (unsigned short *)0xb8000;
+    for (int i = 0; str[i] != '\0'; ++i)
+    {
         VideoMemory[i] = (VideoMemory[i] & 0xFF00) | str[i];
     }
 }
-extern "C" void kernelMain(void* multiboot_structure, unsigned int magicnumber){
+
+typedef void (*constructor)();
+
+extern "C" constructor start_ctors;
+extern "C" constructor end_ctors;
+extern "C" void callConstructors()
+{
+    for (constructor *i = &start_ctors; i != end_ctors; i++)
+    {
+       
+    }
+}
+
+extern "C" void kernelMain(void *multiboot_structure, unsigned int magicnumber)
+{
     printf("Hello  Kernel");
-    while(1);
+    while (1)
+        ;
 }
